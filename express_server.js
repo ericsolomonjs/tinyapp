e8ae7ee3d2e086ab02597f8139a548cd387b9172
facelessagony <eric.solomon.ca@gmail.com>
2022-11-03 20:42:39 +0000

    added delete functionality for short urls

--- express_server.js
+++ express_server.js
@@ -1,17 +1,14 @@
 const express = require("express");
-
 const app = express();
 const PORT = 8080; // default port 8080
-
 //set EJS as view engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 //middleware for decoding buffers
 app.use(express.urlencoded({ extended: true }));
 
 function generateRandomString() {
   return Math.random().toString(36).slice(6);
 }
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -31,23 +28,27 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls_new", (req, res) => {
+app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// //POST new url route
-// app.post("/urls/new", (req, res) => {
-//   let fnLongURL = res.longURL;
-//   res.statusCode = 200;
-//   urlDatabase[generateRandomString()] =  fnLongURL;
-// })
+//POST new url route
+app.post("/urls/new", (req, res) => {
+  let fnLongURL = res.longURL;
+  res.statusCode = 200;
+  urlDatabase[generateRandomString()] =  fnLongURL;
+})
 
 app.post("/urls", (req, res) => {
-  // console.log(req.body);
   res.statusCode = 200;
-  let randomString = generateRandomString()
-  urlDatabase[randomString] = res.longURL; // store the new short and long urls
-  res.redirect("/urls/"+randomString); // redirect to the links page
+  let randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL; // store the new short and long urls
+  res.redirect("/urls/"+randomString); // redirect to the link's page
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
 });
 
 app.get("/urls/:id", (req, res) => {
